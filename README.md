fd2b154a5298351cc8173f0b29d35fa1cd858e87
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-10-31 20:00:20 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Criando uma função Soma
+
+- Criei uma função que soma 2  números inteiros. 
+
+- Criei a função de multiplicação de 2 número inteiros
---
+++ index.js
@@ -0,0 +1,20 @@
+function soma (a, b){
+    return a + b;
+}
+
+
+function mult (c, d){
+    return c * d;
+}
+
+function div (e, f){
+    return e / f;
+}
+
+function  sub (g, h){
+    return g - h;
+}
+
+function pot (i, j){
+    return i ** j;
+}
